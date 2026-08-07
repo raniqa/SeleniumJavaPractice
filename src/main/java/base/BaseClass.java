2a21d1ec1e564a7ed6bef0f1ab68beaaaa1f420d
Rani <ranirahulpn@gmail.com>
2026-08-07 17:44:20 +0530

    Add updated BaseClass

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -1,14 +1,28 @@
 package base;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseClass {
 
     protected WebDriver driver;
 
     public void launchBrowser() {
-        driver = new ChromeDriver();
+    	ChromeOptions options = new ChromeOptions();
+
+        Map<String, Object> prefs = new HashMap<>();
+
+        // Disable Chrome password manager
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+
+        options.setExperimentalOption("prefs", prefs);
+    	
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 
